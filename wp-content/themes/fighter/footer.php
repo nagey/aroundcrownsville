70666afb9da21cd53e1972a0de8036c4fb2a7ba9
Stefan Nagey <stefan41@gmail.com>
2008-12-13 13:12:26 +0000

    footer change to fighter theme

--- wp-content/themes/fighter/footer.php
+++ wp-content/themes/fighter/footer.php
@@ -2,7 +2,7 @@
 <div id="footer">
 		<p id="footertext">
 
-		<?php bloginfo('name'); ?> is powered by <a href='http://wordpress.org'>Wordpress</a>.<br />Fighter theme designed by <a href='http://www.mydizayn.org'>My Dizayn</a>.
+		
 
 </p>
 		</div>
